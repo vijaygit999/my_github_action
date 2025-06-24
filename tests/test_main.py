91169c52c130e93e56c17d7541da181ace40c040
vijaygit999 <127684730+vijaygit999@users.noreply.github.com>
2025-06-24 16:58:51 +0530

    Update test_main.py
    2nd time error is fixed

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
-from ..app.main import add
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.main import add
 
 def test_add():
     assert add(2, 3) == 5
